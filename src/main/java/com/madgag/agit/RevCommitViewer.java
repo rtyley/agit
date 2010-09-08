781e32037f1088aa38033101e53b2ba2417d3954
roberto <roberto.tyley@guardian.co.uk>
2010-09-08 23:45:18 +0100

    Got the expandable diffentry list to look almost humanish

--- src/main/java/com/madgag/agit/RevCommitViewer.java
+++ src/main/java/com/madgag/agit/RevCommitViewer.java
@@ -1,13 +1,9 @@
 package com.madgag.agit;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 import org.eclipse.jgit.diff.DiffEntry;
-import org.eclipse.jgit.errors.CorruptObjectException;
-import org.eclipse.jgit.errors.IncorrectObjectTypeException;
-import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -15,18 +11,25 @@ import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepository;
 import org.eclipse.jgit.treewalk.TreeWalk;
-import org.eclipse.jgit.treewalk.filter.AndTreeFilter;
 import org.eclipse.jgit.treewalk.filter.TreeFilter;
 
-import android.app.Activity;
+import android.app.ExpandableListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
 
-public class RevCommitViewer extends Activity {
-    private File gitdir;
+public class RevCommitViewer extends ExpandableListActivity {
+    
+
+	private File gitdir;
+	private MyExpandableListAdapter mAdapter;
+	private List<DiffEntry> files;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,11 +58,16 @@ public class RevCommitViewer extends Activity {
 				tw.addTree(commitParentTree);
 				tw.addTree(revWalk.parseTree(commit.getTree()));
 				tw.setFilter(TreeFilter.ANY_DIFF);
-				List<DiffEntry> files = DiffEntry.scan(tw);
+				files = DiffEntry.scan(tw);
 				Log.i("RCCV",files.toString());
-				ListView listView=(ListView) findViewById(R.id.commit_view_diffs_list);
-				listView.setAdapter(new ArrayAdapter<DiffEntry>(this, android.R.layout.simple_list_item_1, files));
 				
+				
+		        mAdapter = new MyExpandableListAdapter(this);
+		        setListAdapter(mAdapter);
+		        
+//				ListView listView=(ListView) findViewById(R.id.commit_view_diffs_list);
+//				listView.setAdapter(new ArrayAdapter<DiffEntry>(this, android.R.layout.simple_list_item_1, files));
+//				
 			}
 
 			
@@ -67,5 +75,76 @@ public class RevCommitViewer extends Activity {
 			e.printStackTrace();
 		}
     }
+    
+    public class MyExpandableListAdapter extends BaseExpandableListAdapter {
 
+    	LayoutInflater mInflater;
+    	
+    	public MyExpandableListAdapter(Context context) {
+    		mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    	}
+    	
+		public Object getChild(int arg0, int arg1) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public long getChildId(int arg0, int arg1) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public View getChildView(int arg0, int arg1, boolean arg2, View arg3,
+				ViewGroup arg4) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public int getChildrenCount(int arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public Object getGroup(int index) {
+			return files.get(index);
+		}
+
+		public int getGroupCount() {
+			// TODO Auto-generated method stub
+			return files.size();
+		}
+
+		public long getGroupId(int arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+	    public View getGroupView(int groupPosition, boolean isExpanded, View convertView,
+	            ViewGroup parent) {
+	        View v;
+	        if (convertView == null) {
+	            v = newGroupView(isExpanded, parent);
+	        } else {
+	            v = convertView;
+	        }
+	        DiffEntry diffEntry = files.get(groupPosition);
+			((TextView) v.findViewById(R.id.commit_file_textview)).setText(diffEntry.getNewPath());
+	        return v;
+	    }
+	    
+		private View newGroupView(boolean isExpanded, ViewGroup parent) {
+	        return mInflater.inflate(isExpanded?R.layout.commit_group_view:R.layout.commit_group_view, parent, false);
+	    }
+
+		public boolean hasStableIds() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		public boolean isChildSelectable(int arg0, int arg1) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+	}
 }
