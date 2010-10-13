289f624656bcd0163181f94f38d7b25a23cb980c
roberto <roberto.tyley@guardian.co.uk>
2010-10-14 00:27:10 +0100

    Improvements to the pom layout, edging towards a proper test

---
+++ agit-tests/src/main/java/com/madgag/agit/CloneActivityTest.java
@@ -0,0 +1,14 @@
+package com.madgag.agit;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+
+public class CloneActivityTest extends ActivityInstrumentationTestCase2<Clone> {
+    public CloneActivityTest() {
+		super(Clone.class);
+	}
+
+	@SmallTest
+    public void testPreconditions() {
+    }
+}
