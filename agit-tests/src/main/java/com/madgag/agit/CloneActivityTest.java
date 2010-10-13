e305f799dd720e9c83a249f296e7b2fdf0be129e
roberto <roberto.tyley@guardian.co.uk>
2010-10-14 00:38:57 +0100

    Got a failing test! YEAH!

--- agit-tests/src/main/java/com/madgag/agit/CloneActivityTest.java
+++ agit-tests/src/main/java/com/madgag/agit/CloneActivityTest.java
@@ -2,13 +2,25 @@ package com.madgag.agit;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.EditText;
 
 public class CloneActivityTest extends ActivityInstrumentationTestCase2<Clone> {
-    public CloneActivityTest() {
+    private EditText editText;
+    
+	public CloneActivityTest() {
 		super(Clone.class);
 	}
 
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        final Clone a = getActivity();
+        editText = (EditText) a.findViewById(R.id.CloneUrlEditText);
+    }
+    
 	@SmallTest
     public void testPreconditions() {
+		assertEquals("", editText.getText().toString());
     }
 }
