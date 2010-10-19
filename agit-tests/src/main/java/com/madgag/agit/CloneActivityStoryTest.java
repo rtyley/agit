ddcd4748b5b64256c52d08edf49bb50345ce18b8
roberto <roberto.tyley@guardian.co.uk>
2010-10-19 20:37:35 +0100

    Tidying...

--- agit-tests/src/main/java/com/madgag/agit/CloneActivityStoryTest.java
+++ agit-tests/src/main/java/com/madgag/agit/CloneActivityStoryTest.java
@@ -2,38 +2,15 @@ package com.madgag.agit;
 
 import java.io.File;
 
-import android.test.suitebuilder.annotation.SmallTest;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-
 import com.github.calculon.CalculonStoryTest;
 
-public class CloneActivityFunctionalTest extends CalculonStoryTest<Clone> {
-	private EditText cloneUrlEditText;
-	private Button button;
+public class CloneActivityStoryTest extends CalculonStoryTest<Clone> {
 	
 	private String existingFolder = File.listRoots()[0].getAbsolutePath();
 
-	public CloneActivityFunctionalTest() {
+	public CloneActivityStoryTest() {
 		super("com.agit",Clone.class);
 	}
-
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		Clone activity = getActivity();
-		Log.i("CloneActivityUnitTest", "Lawks "+activity);
-		cloneUrlEditText = (EditText) activity.findViewById(R.id.CloneUrlEditText);
-		button = (Button) activity.findViewById(R.id.GoCloneButton);
-		
-	}
-
-	@SmallTest
-	public void testPreconditions() {
-		assertEquals("", cloneUrlEditText.getText().toString());
-	}
-
 	
     public void testCloneButtonDisabledIfGitDirAlreadyExists() {
         assertThat(R.id.CloneUrlEditText).setText(existingFolder).implies(R.id.GoCloneButton).isDisabled();
