245c92804c72d442f0794485e82fe18c72749098
roberto <roberto.tyley@guardian.co.uk>
2010-10-22 00:45:24 +0100

    Can I  have a dump test for the SO?

--- agit-tests/src/main/java/com/madgag/agit/CloneActivityStoryTest.java
+++ agit-tests/src/main/java/com/madgag/agit/CloneActivityStoryTest.java
@@ -16,14 +16,20 @@ public class CloneActivityStoryTest extends CalculonStoryTest<Clone> {
 	}
 	
     public void testCloneButtonDisabledAndWarningShownIfGitDirAlreadyExists() {
-    	setUp(R.id.UseDefaultGitDirLocation).uncheck().now();
+    	setUp(R.id.UseDefaultGitDirLocation).setChecked(false).now();
         assertThat(R.id.GitDirEditText).setText(existingFolder).implies(R.id.GoCloneButton).isDisabled();
         assertThat(R.id.GitDirWarning).isVisible();
     }
     
     public void testWarningNotShownIfGitDirDoesNotAlreadyExist() {
-    	setUp(R.id.UseDefaultGitDirLocation).uncheck().now();
+    	setUp(R.id.UseDefaultGitDirLocation).setChecked(false).now();
         assertThat(R.id.GitDirEditText).setText(newFolder()).implies(R.id.GitDirWarning).isInvisible();
+    }
+    
+    public void testDoesNotCrashDueToStackOverflow() {
+    	setUp(R.id.UseDefaultGitDirLocation).setChecked(false).now();
+    	setUp(R.id.CloneUrlEditText).setText("/example/project").now();
+        assertThat(R.id.UseDefaultGitDirLocation).setChecked(true).implies(R.id.UseDefaultGitDirLocation).isEnabled();
     }
 
 	private String newFolder() {
