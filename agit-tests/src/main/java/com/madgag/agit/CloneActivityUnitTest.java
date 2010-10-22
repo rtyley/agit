da67d3ea1208083341d5a2fec8bacf35c3393c5d
roberto <roberto.tyley@guardian.co.uk>
2010-10-22 22:44:08 +0100

    Some of those unit tests that you keep hearing people talk about
    These two tests are specifically around launching the 'clone configuration' activity - they should respect the data in the launching intent if it's present.

---
+++ agit-tests/src/main/java/com/madgag/agit/CloneActivityUnitTest.java
@@ -0,0 +1,62 @@
+package com.madgag.agit;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Checkable;
+import android.widget.EditText;
+
+import com.github.calculon.CalculonUnitTest;
+import com.github.calculon.predicate.Predicate;
+
+public class CloneActivityUnitTest extends CalculonUnitTest<Clone> {
+	
+	public CloneActivityUnitTest() {
+		super(Clone.class);
+	}
+	
+	public void testUsesDefaultGitDirLocationIfOnlySourceUriIsProvidedInIntent() {
+		Bundle bundle = new Bundle();
+		final String cloneSourceUri="/example/apple";
+		bundle.putString("source-uri", cloneSourceUri);
+		
+		startActivity(bundle);
+		assertThat(R.id.CloneUrlEditText).satisfies(new Predicate<View>() {
+			public boolean check(View target) {
+				return ((EditText) target).getText().toString().equals(cloneSourceUri);
+			}
+		});
+		
+		assertThat(R.id.UseDefaultGitDirLocation).satisfies(new Predicate<View>() {
+			public boolean check(View target) {
+				return ((Checkable) target).isChecked()==true;
+			}
+		});
+	}
+	
+	public void testUsesSpecifiedGitDirLocationFromIntentIfSupplied() {
+		Bundle bundle = new Bundle();
+		final String cloneSourceUri="/example/apple";
+		final String gitdir="/sdcard/tango";
+		bundle.putString("source-uri", cloneSourceUri);
+		bundle.putString("gitdir", gitdir);
+		
+		startActivity(bundle);
+		assertThat(R.id.CloneUrlEditText).satisfies(new Predicate<View>() {
+			public boolean check(View target) {
+				return ((EditText) target).getText().toString().equals(cloneSourceUri);
+			}
+		});
+		
+		assertThat(R.id.UseDefaultGitDirLocation).satisfies(new Predicate<View>() {
+			public boolean check(View target) {
+				return ((Checkable) target).isChecked()==false;
+			}
+		});
+		
+		assertThat(R.id.GitDirEditText).satisfies(new Predicate<View>() {
+			public boolean check(View target) {
+				return ((EditText) target).getText().toString().equals(gitdir);
+			}
+		});
+	}
+}
