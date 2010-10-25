1c8306ba4a7863d88b4904173db7a1cd1a1e1f1e
roberto <roberto.tyley@guardian.co.uk>
2010-10-25 21:42:11 +0100

    A bit more debug - I don't understand why this is failing

--- agit-tests/src/main/java/com/madgag/agit/GitOperationsServiceTest.java
+++ agit-tests/src/main/java/com/madgag/agit/GitOperationsServiceTest.java
@@ -38,6 +38,7 @@ public class GitOperationsServiceTest extends ServiceTestCase<GitOperationsServi
 		Notification notification = gitOperation.get();
         assertNotNull(notification);
         Repository repository = repositoryOperationContext.getRepository();
+        Log.i(TAG, "After clone: repo directory="+repository.getDirectory()+" workTree="+repository.getWorkTree());
         assertTrue(repository.hasObject(ObjectId.fromString( "9e0b5e42b3e1c59bc83b55142a8c50dfae36b144")));
         assertFalse(repository.hasObject(ObjectId.fromString("111111111111111111111111111111111111cafe")));
 	}
