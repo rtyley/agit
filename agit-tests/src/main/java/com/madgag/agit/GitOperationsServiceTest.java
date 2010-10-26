f696ed20d6d9b45c9a531c04b2b66c5bbc11a159
roberto <roberto.tyley@guardian.co.uk>
2010-10-26 23:01:38 +0100

    Tidy

--- agit-tests/src/main/java/com/madgag/agit/GitOperationsServiceTest.java
+++ agit-tests/src/main/java/com/madgag/agit/GitOperationsServiceTest.java
@@ -7,7 +7,6 @@ import static org.eclipse.jgit.lib.Constants.DOT_GIT;
 
 import java.io.File;
 
-import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.URIish;
