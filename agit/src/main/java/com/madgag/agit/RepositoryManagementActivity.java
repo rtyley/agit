310cf87849c21122c29b71f2ba9325acededa1ed
Roberto Tyley <roberto.tyley@guardian.co.uk>
2011-01-18 01:00:36 +0000

    Use the out-of-the-box icon for deletion...

--- agit/src/main/java/com/madgag/agit/RepositoryManagementActivity.java
+++ agit/src/main/java/com/madgag/agit/RepositoryManagementActivity.java
@@ -97,7 +97,7 @@ public class RepositoryManagementActivity extends RepositoryActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
     	super.onCreateOptionsMenu(menu);
-        menu.add(0, DELETE_ID, 0, R.string.delete_repo_menu_option).setShortcut('0', 'd');
+        menu.add(0, DELETE_ID, 0, R.string.delete_repo_menu_option).setShortcut('0', 'd').setIcon(android.R.drawable.ic_menu_delete);
         return true;
     }
     
