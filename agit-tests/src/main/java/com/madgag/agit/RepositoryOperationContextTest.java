cdec150259f526f61acf58a98d0e0c80fcb99676
Roberto Tyley <roberto.tyley@guardian.co.uk>
2011-03-09 23:54:32 +0000

    tidy

--- agit-tests/src/main/java/com/madgag/agit/RepositoryOperationContextTest.java
+++ agit-tests/src/main/java/com/madgag/agit/RepositoryOperationContextTest.java
@@ -7,13 +7,10 @@ import static org.eclipse.jgit.lib.Constants.DOT_GIT;
 
 import java.io.File;
 
-import org.eclipse.jgit.storage.file.FileRepository;
 import org.eclipse.jgit.transport.URIish;
 
 import roboguice.test.RoboServiceTestCase;
-
 import android.content.Intent;
-import android.test.ServiceTestCase;
 import android.util.Log;
 
 public class RepositoryOperationContextTest extends RoboServiceTestCase<GitOperationsService, AgitApplication> {
