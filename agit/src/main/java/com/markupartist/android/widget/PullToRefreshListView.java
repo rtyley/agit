a73862f5c811dcac818de47ddb4a198529b13055
Roberto Tyley <roberto.tyley@guardian.co.uk>
2011-04-04 22:18:15 +0100

    reduce dex mem to half a gb, prob don't really need so much

---
+++ agit/src/main/java/com/markupartist/android/widget/PullToRefreshListView.java
@@ -0,0 +1,11 @@
+package com.markupartist.android.widget;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: roberto
+ * Date: 04/04/11
+ * Time: 20:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class PullToRefreshListView {
+}
