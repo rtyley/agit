4481858bcd44d3f98d8713a3d70e8da94a02669e
Roberto Tyley <roberto.tyley@guardian.co.uk>
2011-04-11 15:29:55 +0100

    Reinstated repo list on dashboard activity

--- agit/src/main/java/com/madgag/agit/DashboardActivity.java
+++ agit/src/main/java/com/madgag/agit/DashboardActivity.java
@@ -2,22 +2,56 @@ package com.madgag.agit;
 
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.*;
 import com.markupartist.android.widget.ActionBar;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
+
+import java.io.File;
+
+import static com.madgag.agit.RepositoryManagementActivity.manageRepoIntent;
 
 public class DashboardActivity extends RoboActivity {
     private static final String TAG = "DA";
 
     @InjectView(R.id.actionbar) ActionBar actionBar;
+    @InjectView(android.R.id.list) ListView listView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
         actionBar.setHomeLogo(R.drawable.actionbar_agit_logo);
+
+        setupRepoList();
+    }
+
+    private void setupRepoList() {
+        // Put a managed wrapper around the retrieved cursor so we don't have to worry about
+        // requerying or closing it as the activity changes state.
+        Cursor mCursor = managedQuery(GitInfoProvider.CONTENT_URI, null, null, null, null);
+
+        // Now create a new list adapter bound to the cursor.
+        // SimpleListAdapter is designed for binding to a Cursor.
+        ListAdapter adapter = new SimpleCursorAdapter(
+                this, // Context.
+                android.R.layout.simple_list_item_1,
+                mCursor,                                              // Pass in the cursor to bind to.
+                new String[] {"gitdir"},           // Array of cursor columns to bind to.
+                new int[] {android.R.id.text1});  // Parallel array of which template objects to bind to those columns.
+
+        // Bind to our new adapter.
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String gitdir=((TextView)view.findViewById(android.R.id.text1)).getText().toString();
+                startActivity(manageRepoIntent(new File(gitdir)));
+            }
+        });
     }
 
     // used by dashboard.xml
