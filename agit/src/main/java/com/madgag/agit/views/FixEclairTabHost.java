eccaa295a48ea5f4e0ea1a8363f0ba01e27bbaf6
Roberto Tyley <roberto.tyley@guardian.co.uk>
2011-06-12 14:33:02 +0100

    Badass Android 2.1 (Eclair) support - fix TabHost NPE
    Was getting an NPE at TabHost.dispatchWindowFocusChanged(TabHost.java:295)
    for Android 2.1. Android 2.2 and greater has been fixed with this commit:
    
    http://android.git.kernel.org/?p=platform/frameworks/base.git;a=blobdiff;f=core/java/android/widget/TabHost.java;h=412f817353a99054ab36234e1441b48c6c7ad820;hp=31920e764e6d25523b6dd5ffb1e4087d4d8fa3b0;hb=acdef59d66094f11da4a6f57194747dc06f73da2;hpb=159f0015418955501d8cf2744b0393db2e73f394
    
    -which basically does a null-check in dispatchWindowFocusChanged()
    
    Fix is to extend TabHost, and simply override the
    dispatchWindowFocusChanged() method with a similar null-check.

---
+++ agit/src/main/java/com/madgag/agit/views/FixEclairTabHost.java
@@ -0,0 +1,25 @@
+package com.madgag.agit.views;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TabHost;
+
+public class FixEclairTabHost extends TabHost {
+
+    public FixEclairTabHost(Context context) {
+        super(context);
+    }
+
+    public FixEclairTabHost(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void dispatchWindowFocusChanged(boolean hasFocus) {
+        if (getCurrentView()!=null) {
+            super.dispatchWindowFocusChanged(hasFocus);
+        }
+    }
+
+}
