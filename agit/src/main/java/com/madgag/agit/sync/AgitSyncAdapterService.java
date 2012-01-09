d465797f808e2123477621d297c2ef033edc60f4
Roberto Tyley <roberto@github.com>
2012-01-09 18:11:29 +0000

    Fix sync service adapter service for RoboGuice 2

--- agit/src/main/java/com/madgag/agit/sync/AgitSyncAdapterService.java
+++ agit/src/main/java/com/madgag/agit/sync/AgitSyncAdapterService.java
@@ -1,17 +1,19 @@
 package com.madgag.agit.sync;
 
+import com.google.inject.Inject;
+
 import android.content.Intent;
 import android.os.IBinder;
-import com.google.inject.Inject;
-import com.google.inject.Provider;
+import roboguice.inject.ContextScopedProvider;
 import roboguice.service.RoboService;
 
 public class AgitSyncAdapterService extends RoboService {
 
-	@Inject Provider<SyncAdapter> syncAdapterProvider;
+    @Inject
+    ContextScopedProvider<SyncAdapter> syncAdapterProvider;
 
-	@Override
-	public IBinder onBind(Intent intent) {
-        return syncAdapterProvider.get().getSyncAdapterBinder();
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        return syncAdapterProvider.get(this).getSyncAdapterBinder();
+    }
 }
