9922158f704755bf5d03388b8f867a3b1eff5f50
Roberto Tyley <roberto@github.com>
2012-02-17 14:25:22 +0000

    Add failing test for the repo viewer
    The RepositoryViewerActivityTest test was formerly empty.

--- agit-integration-tests/src/main/java/com/madgag/agit/RepositoryViewerActivityTest.java
+++ agit-integration-tests/src/main/java/com/madgag/agit/RepositoryViewerActivityTest.java
@@ -1,15 +1,45 @@
+/*
+ * Copyright (c) 2011 Roberto Tyley
+ *
+ * This file is part of 'Agit' - an Android Git client.
+ *
+ * Agit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Agit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.madgag.agit;
 
-import android.test.suitebuilder.annotation.SmallTest;
-import roboguice.test.RoboActivityUnitTestCase;
+import static com.madgag.agit.AndroidTestEnvironment.helper;
+import static com.madgag.agit.RepositoryViewerActivity.manageRepoIntent;
 
-public class RepositoryViewerActivityTest extends RoboActivityUnitTestCase<RepositoryViewerActivity> {
-    public RepositoryViewerActivityTest() {
-        super(RepositoryViewerActivity.class);
-    }
+import org.eclipse.jgit.lib.Repository;
 
-    @SmallTest
-	public void testShouldTitleFromRepo() throws Exception {
+import android.test.ActivityInstrumentationTestCase2;
 
-    }
+public class RepositoryViewerActivityTest extends ActivityInstrumentationTestCase2<RepositoryViewerActivity> {
+
+	private final static String TAG = RepositoryViewerActivityTest.class.getSimpleName();
+
+	public RepositoryViewerActivityTest() {
+		super("com.madgag.agit",RepositoryViewerActivity.class);
+	}
+	
+	public void testShouldShowRepoViewerPageWithoutExplosion() throws Exception {
+        Repository repoWithTags = helper(getInstrumentation()).unpackRepo("small-repo.with-tags.zip");
+		
+		setActivityIntent(manageRepoIntent(repoWithTags.getDirectory()));
+		
+		getActivity(); // shouldn't crash
+	}
+	
 }
